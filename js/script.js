87d091127ece6d7c0bbe9c52be92f9111eb3ca3e
Rod Rice <rodrice@swbell.net>
2019-01-07 16:00:40 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,19 +2,15 @@
 // January 2019
 // Learner: Rod Rice
 
-// declare variable for background color function (exceeds item)
+// declare variable for background color function (an exceeds item)
 var bgColor = "";
-// An array of quote objects to display randomly with tags (exceeds item)
+
+// An array of quote objects to display randomly with tags (an exceeds item)
 var quotes = [
   {
     quote: "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-    source: "Martin Fowler",
+    source: "Martin Fowler, Chief Scientist at Thoughtworks",
     tags: ["humor", "coding"]
-  },
-  {
-    quote: "However difficult life may seem, there is always something you can do and succeed at. It matters that you don't just give up.",
-    source: "Stephen Hawking",
-    tags: ["inspirational", "Hawking"]
   },
   {
     quote: "Just because I do a lot of thinking doesn't mean I don't like parties and getting into trouble.",
@@ -25,7 +21,7 @@ var quotes = [
   },
   {
     quote: "Every great developer you know got there by solving problems they were unqualified to solve until they actually did it.",
-    source: "Patrick McKenzie",
+    source: "Patrick McKenzie, Kalkzumeus Software",
     citation: "Twitter",
     year: "2016",
     tags: ["coding", "learning"]
@@ -39,24 +35,18 @@ var quotes = [
   },
   {
     quote: "Whether we're fighting climate change or going to space, everything is moved forward by computers, and we don't have enough people who can code. Teaching young people to code early on can build skills and confidence, and energize the classroom with learning-by-doing opportunities.",
-    source: "Richard Branson",
+    source: "Richard Branson, Founder of Virgin Group",
     tags: ["learning", "Branson"]
   },
   {
     quote: "Computer programming is quickly becoming an essential career skill. Learning to code is a fantastic opportunity equalizer- if you're good at it, it can help you achieve your dreams.",
-    source: "Cory Booker",
+    source: "Cory Booker, US Senator",
     tags: ["coding", "learning", "Booker"]
   },
   {
     quote: "To be prepared for the demands of the 21st century and to take advantage of its opportunities, it is essential that more of our students today learn basic computer programming skills, no matter what field of work they want to pursue.",
     source: "Todd Park, US Chief Technology Officer",
     tags: ["learning", "coding", "Park"]
-
-  },
-  {
-    quote: "Creativity is just connecting things. When you ask creative people how they did something they feel a little guilty because they didnt really do it, they just saw something. It seemed obvious to them after a while.",
-    source: "Steve Jobs",
-    tags: ["learning", "creativity", "Jobs"]
 
   },
   {
@@ -71,12 +61,42 @@ var quotes = [
   },
   {
     quote: "Learning to write programs stretches your mind, helps you think better, and creates a way of thinking about things that I think is helpful in all domains.",
-    source: "Bill Gates",
+    source: "Bill Gates, Cofounder of Microsoft",
     tags: ["learning", "coding", "Gates"]
+  },
+  {
+    quote: "Our policy at Facebook is literally to hire as many talented engineers as we can find. There just aren't enough people who are trained and have these skills today.",
+    source: "Mark Zuckerberg, Facebook",
+    tags: ["training", "Zuckerberg"]
+  },
+  {
+    quote: "Learning to code is the single best thing anyone can do to get the most out of the amazing future in front of us.",
+    source: "Marc Andreessen, cofounder of Netscape",
+    tags: ["coding", "learning", "Andreessen"]
+  },
+  {
+    quote: "Coding is today's language of creativity. All our children deserve a chance to become creators instead consumers of science.",
+    source: "Maria Klawe, Computer Scientist",
+    tags: ["learning", "coding", "Klawe"]
+  },
+  {
+    quote: "Learning to code will be a huge booster for your future, no matter what your professional plans may be. Learning to code will also make you extremely cool!",
+    source: "Max Levchin, Co-Founder of Paypal",
+    tags: ["learning", "Levchin"]
+  },
+  {
+    quote: "Great programming is not all that dissimilar to great art. Once you start thinking in concepts of programming it makes you a better person... as does learning a foreign language, as does learning math, as does learning how to read.",
+    source: "Jack Dorsey, Creator of Twitter and Square",
+    tags: ["learning", "Dorsey"]
+  },
+  {
+    quote: "Coders change the world. They build new, amazing things faster than ever before. Anyone with imagination can learn to write code.",
+    source: "Jeff Wilke, CEO Amazon",
+    tags: ["coding", "Wilke"]
   }
 ]
 
-// Function to create random background color (exceeds item)
+// Function to create random background color (an exceeds item)
 function random_bg_color() {
   var r = Math.floor(Math.random() * 256);
   var g = Math.floor(Math.random() * 256);
@@ -95,7 +115,7 @@ function getRandomQuote(){
 function printQuote() {
   var displayedQuote = getRandomQuote();
   var message = "";
-  if (displayedQuote.citation === undefined || displayedQuote.year === undefined) {
+    if (displayedQuote.citation === undefined || displayedQuote.year === undefined) {
     message += "<p class='quote'>" + displayedQuote.quote + "</p>";
     message += "<p class='source'>" + displayedQuote.source;
 }
@@ -104,8 +124,6 @@ function printQuote() {
     message += "<p class = 'source'>" + displayedQuote.source;
     message += "<span class='citation'>" + displayedQuote.citation + "</span>";
     message += "<span class = 'year'>" + displayedQuote.year + "</span>" + "</p>";
-
-
 }
 
   document.getElementById('quote-box').innerHTML = message;
@@ -115,7 +133,7 @@ function printQuote() {
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
-//Auto change quote display at set interval (exceeds item)
+//Auto change quote display at set interval (an exceeds item)
 var quoteRefreshInterval = setInterval(printQuote, 10000);
 
 printQuote();
